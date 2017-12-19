f8c7e9c30d3ff6faa2c0e10e9c9de9f3c26060ba
Sean6bucks <sean6bucks@gmail.com>
2017-12-19 14:03:21 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="sk-loader">
+				<header className="header">
+					<h1 className="title">Welcome to React22</h1>
+				</header>
+			</div>
+		);
+	}
+}
+
+export default App;
